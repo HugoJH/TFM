f86c8b3c79dbe674e2242efb3dcc6fda423c518c
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2017-09-06 15:20:42 +0200

    Added mock data and red tests for fingerprinter

--- drugdealer/utils.py
+++ drugdealer/utils.py
@@ -1,4 +1,17 @@
 
 def formatRecord(OneRecordDataframe):
-	pass
+    mol = Chem.MolFromSmiles(DFRow['canonical_smiles'])
+    bson_encoded = ({"CHEMBL_ID": DFRow['chembl_id'],
+                     "MOL": (dumps(mol)),
+                     "FingerPrint_AtomPairs": (dumps(self.fingerprinter.getAtomPairsFingerPrint(mol))),
+                     "FingerPrint_Topological": (dumps(self.fingerprinter.getTopologicalFingerPrint(mol))),
+                     "FingerPrint_Morgan": (dumps(self.fingerprinter.getMorganFingerPrint(mol))),
+                     "Properties": {"ALOGP": DFRow['alogp'],
+                                    "PSA": DFRow['psa'],
+                                    "HBA": DFRow['hba'],
+                                    "HBD": DFRow['hbd'],
+                                    "RTB": DFRow['rtb']
+                                   },
+                     "Targets": []
+                    })
 
---
+++ tests/test_fingerprinter.py
@@ -0,0 +1,31 @@
+from drugdealer.FingerPrinter import FingerPrinter
+
+class testFingerPrintCreator():
+    
+    def setUp(self):
+        self.sampleSMILE = "Cc1cc(ccc1C(=O)c2ccccc2Cl)N3N=CC(=O)NC3=O"
+        self.sampleInchi = "OWRSAHYFSSNENM-UHFFFAOYSA-N"
+        
+        AtomPairsFile = open("mock/AtomPairsFingerPrint.pickle","rb")
+        self.atomPairsFingerPrint = pickle.load(file=AtomPairsFile)
+        AtomPairsFile.close()
+        
+        MorganFile = open("mock/MorganFingerPrint.pickle","rb")
+        self.morganFingerPrint = pickle.load(file=MorganFile)
+        MorganFile.close()
+        
+        TopologicalFile = open("mock/TopologicalFingerPrint.pickle","rb")
+        self.topologicalFingerPrint = pickle.load(file=TopologicalFile)
+        TopologicalFile.close()
+        
+        self.FingerPrintCreator = FingerPrintCreator()
+    
+    def test_getTopologicalFingerPrint(self):
+    	pass
+
+    def test_getMorganFingerPrint(self):
+    	pass
+
+    def test_getAtomPairsFingerPrint(self):
+        pass
+        
