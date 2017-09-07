9264e607e9346f7ee57f8066e784659230f46d93
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2017-09-07 11:37:31 +0200

    Changed path to dbs formatting

--- tests/test_DBPopulator.py
+++ tests/test_DBPopulator.py
@@ -2,11 +2,14 @@ from sqlite3 import connect
 from pathlib import Path
 from pandas import read_sql
 from drugdealer.utils import formatRecord
+from os.path import join,dirname
 
 def test_formatRecord():
-    pathToDB = str(Path("../DBs/chembl_22_1.db").resolve())
-    pathToSampleMongoRecord = str(Path("mock/sampleMongoRecord.json").resolve()) 
+    pathToDB = str(Path(join(dirname(__file__),"../DBs/chembl_22_1.db")).resolve())
+    pathToSampleMongoRecord = str(Path(join(dirname(__file__), "mock/sampleMongoRecord.json")).resolve()) 
+    print (pathToDB)
     con = connect(pathToDB)
+
     sql_test_query = """ SELECT distinct chembl_id, entity_type, alogp, hba, hbd, psa, rtb, canonical_smiles
                               FROM chembl_id_lookup  inner join compound_structures 
                               on (entity_type='COMPOUND' and entity_id = compound_structures.molregno)
